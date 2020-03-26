7d985afc83556e9b633c31912f3ac7be53b34984
Erik Ostlund <erik.ostlund@infinitecampus.com>
2020-03-26 10:18:16 -0500

    In the Interceptor: modify the request object

--- src/app/auth-interceptor.service.ts
+++ src/app/auth-interceptor.service.ts
@@ -4,6 +4,15 @@ export class AuthInterceptorService implements HttpInterceptor {
     // this takes 2 params: a request and a forwarding (next): so it can continue
     intercept(req: HttpRequest<any>, next: HttpHandler) {
         console.log("Request is on the way!");
-        return next.handle(req);
+        console.log('Request URL: ', req.url);
+
+        // can modify the request. But it is immutable so to modify it, do this:
+        const modifiedRequest = req.clone(
+            {
+                // url: 'some new url',
+                headers: req.headers.append('Auth', 'abc')
+            }
+        );
+        return next.handle(modifiedRequest);
     }
 }
